e483dbc763e2bcb3098ea8f88a35b7172a09d5e3
Gunawan-aji <gunawanwijayanto533@gmail.com>
2026-05-28 15:58:13 +0700

    fix: remove named parameter from trustProxies

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         // Menyuruh Laravel mempercayai proxy dari Coolify
-    $middleware->trustProxies(at: '*');
+        $middleware->trustProxies('*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
