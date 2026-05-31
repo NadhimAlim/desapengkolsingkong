b8292ab7d865e2c6a5ce3b085862eb1c77cb4d08
nadhimalimmuhammad <tvnadhim@gmail.com>
2026-05-31 20:01:02 +0700

    menambahkan seo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'UMKM Singkong Desa Pengkol')</title>
+    @yield('seo_meta')
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body class="site-body">
